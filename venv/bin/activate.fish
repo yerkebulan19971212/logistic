b290624875fcb3d3ac66638806d79aac1a0cb63f
yerkebulan19971212 <yerkebulan19971212@gmail.com>
2021-03-16 17:35:29 +0600

    Conflicts

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/beka/oobb/logistic/venv"
+set -gx VIRTUAL_ENV "/home/abyer/PycharmProjects/Carservice/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
